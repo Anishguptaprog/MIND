96a4f226936cb144cbe38802f6adada18e6cf26f
Anishguptaprog <81504815+Anishguptaprog@users.noreply.github.com>
2022-03-17 23:00:18 +0530

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int main()
+{	ll n,k;
+	cin>>n;
+	for(int i=n/2;i>-(n/2)-1;i--)
+	{	if(i<0)
+		k=i*-1;
+		else
+		k=i;
+		for(int j=0;j<=k;j++)
+		{	cout<<" ";
+		}
+		for(int j=0;j<n-2*k;j++)
+		{	cout<<"*";
+		}
+		for(int j=0;j<=k;j++)
+		{	cout<<" ";
+		}
+		cout<<endl;
+	}
+
+
+return 0;
+}
