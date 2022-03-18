3c129183dccd8f7752ce26a6aef5cca4b0f192cb
Anishguptaprog <81504815+Anishguptaprog@users.noreply.github.com>
2022-03-18 22:39:02 +0530

    Update Pattern4.cpp

--- Pattern4.cpp
+++ Pattern4.cpp
@@ -1,42 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-
-void fib(int f[], int N)
-{
-    
-    f[1] = 0;
-    f[2] = 1;
- 
-    for (int i = 3; i <= N; i++)
-        f[i] = f[i - 1] + f[i - 2];
-}
- 
-void fibTriangle(int n)
-{
-    
-    int N = n * (n + 1) / 2;
-    int f[N + 1];
-    fib(f, N);
- 
-    
-    int Num = 1;
- 
-    
-    for (int i = 1; i <= n; i++) {
-       
-        for (int j = 1; j <= i; j++)
-            cout << f[Num++] << " ";
- 
-        cout << endl;
-    }
-}
- 
 
 int main()
 {
     int n ;
     cin>>n;
-    fibTriangle(n);
+    int a=1,b=0,c=0;
+    for(int i=0;i<n;i++)
+    {  for(int j=0;j<i+1;j++)
+      {   cout<<c<<" ";
+          b=a;
+          a=c;
+          c=a+b;
+      }
+     cout<<endl;
+    }
     return 0;
 }
