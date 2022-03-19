e1a11ce34df89b8feef1902daa15e548a8e6b3c4
Anishguptaprog <81504815+Anishguptaprog@users.noreply.github.com>
2022-03-19 22:08:45 +0530

    Create Pattern8.cpp

---
+++ Pattern8.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+void swastika(int row, int col)
+{
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++) {
+         
+    
+    if (i < row / 2) {
+         
+       
+        if (j < col / 2) {
+            
+        if (i == 0 && i < row / 2)
+            cout << "*" << " ";
+             
+        else
+            cout << " " << " ";
+        }
+         
+        else if (j == col / 2)
+        cout << "*";
+        else {
+             
+       
+        if (i < row / 2 && j < col - 1)
+            cout << " " << " ";
+        if (j == col - 1)
+            cout << " "    << "*";
+        }
+    }
+ 
+    
+    else if (i == row / 2)
+        cout << "*" << " ";
+    else
+    {
+        if (j == col / 2 || j == 0)
+        cout << "*" << " ";
+        else if (i == row - 1)
+        {
+    
+        if (j <= col / 2 || j == col)
+            cout << " " << " ";
+        else
+            cout << "*" << " ";
+        }
+         
+        else
+        cout << " " << " ";
+    }
+    }
+    cout << "\n";
+}
+}
+ 
+int main()
+{
+  int n;
+  cin>>n;
+  
+  swastika(n, n);
+ 
+   return 0;
+}
