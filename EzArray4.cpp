4fe64bd413293af5b1a23829c7e33d6ec351310e
Anishguptaprog <81504815+Anishguptaprog@users.noreply.github.com>
2022-03-31 08:46:14 +0530

    Create EzArray4.cpp

---
+++ EzArray4.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define long long ll
+using namespace std;
+void rotate(int arr[], int n)
+{
+    int i = 0, j = n-1; 
+      while(i != j){
+      swap(arr[i], arr[j]);
+      i++;
+    }
+}
+ 
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5}, i;
+    int n = sizeof(arr) /
+            sizeof(arr[0]);
+ 
+ 
+    rotate(arr, n);
+ 
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+ 
+    return 0;
+}
